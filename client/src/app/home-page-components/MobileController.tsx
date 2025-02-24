9d85bf6a6d9195a5168ab71baadff793ecf48920
Agustin Gil <agustingil83@gmail.com>
2025-02-23 22:44:52 -0300

    Minor changes

--- client/src/app/home-page-components/MobileController.tsx
+++ client/src/app/home-page-components/MobileController.tsx
@@ -1,14 +1,22 @@
 import { FaShare } from 'react-icons/fa';
 import { MobileControllerBtn } from './MobileControllerBtn';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { MobileControllerShareBtn } from './mobile-controller-btns/MobileControllerShareBtn';
 import { MobileControllerShareWhatsappBtn } from './mobile-controller-btns/MobileControllerShareWhatsappBtn';
 import { MobileControllerLikeBtn } from './mobile-controller-btns/MobileControllerLikeBtn';
 import { MobileControllerSaveBtn } from './mobile-controller-btns/MobileControllerSaveBtn';
+import { useAppsStore } from '../infrastructure/stores/useAppStore';
 
 export const MobileController = () => {
+  const { mobilePinControllerRotation } = useAppsStore();
+
   return (
-    <div className='p-2 w-[130px] h-[130px] rounded-full mr-10 flex justify-center items-center z-0 relative mt-16'>
+    <div
+      className='p-2 w-[130px] h-[130px] rounded-full mr-10 flex justify-center items-center z-0 relative mt-16'
+      style={{
+        rotate: `${mobilePinControllerRotation}deg`,
+      }}
+    >
       <div className=' outline-3 outline outline-slate-400 min-w-[35px] min-h-[35px] w-[35px] h-[35px] rounded-full opacity-40'></div>
       <MobileControllerLikeBtn />
       <MobileControllerSaveBtn />
